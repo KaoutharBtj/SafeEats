517abf778c0afeabdc8423cdeee850068bf2a7ec
BOUTJANOUITE Kaouthar <boutjanouitek@gmail.com>
2025-02-09 02:55:54 +0100

    Delete OneDrive/Bureau/SafeEats-main directory

--- OneDrive/Bureau/SafeEats-main/backend/src/main/java/com/app/allergy_alert_be/service/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.app.allergy_alert_be.service;
-
-public class UserNotFoundException extends Exception {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
