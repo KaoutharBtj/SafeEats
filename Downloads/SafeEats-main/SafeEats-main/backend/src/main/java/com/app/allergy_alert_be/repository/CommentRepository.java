695a1892cf83d4899319b62c1ded215e21757c87
BOUTJANOUITE Kaouthar <boutjanouitek@gmail.com>
2025-02-09 03:06:31 +0100

    Delete Downloads/SafeEats-main/SafeEats-main directory

--- Downloads/SafeEats-main/SafeEats-main/backend/src/main/java/com/app/allergy_alert_be/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.allergy_alert_be.repository;
-
-import com.app.allergy_alert_be.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
--- Downloads/SafeEats-main/SafeEats-main/frontend/src/pages/About_Us.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const About_Us = () =>{
-    return <div>About Us</div>
-}
